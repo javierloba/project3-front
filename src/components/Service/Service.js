385dacf4fc18e1e5e23cd95e05f0de1e76b833d2
QuimRobres <quim.robres@gmail.com>
2021-05-15 13:01:15 +0200

    service item and show service done

--- src/components/Service/Service.js
+++
@@ -1,4 +0,0 @@
-import React, { Component } from 'react'
-import privateService from '../../services/private.service'
-
-export default function Service({name, })
---
+++ src/components/ServiceList/ServiceList.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+import publicService from "../../services/public.service";
+import ServiceItem from "../ServiceItem/ServiceItem";
+
+export default class ServiceList extends Component {
+    constructor(props){
+        super(props)
+        this.state= {
+            services: []
+        }
+        this.publicService = new publicService();
+    }
+
+    refreshState() {
+        this.publicService.showServices()
+        .then(response => {
+            this.setState({ todos: response.data })
+        })
+        .catch(err => console.error(err))
+    }
+
+    componentDidMount(){
+        this.refreshState();
+    }
+
+    displayServices(){
+        const { services } = this.state;
+        return services.map(service => {
+            return(
+                <ServiceItem {...service} key={service.id} />
+            )
+        })
+
+    }
+
+    render() {
+        return (
+            <div>
+                {this.displayServices()};
+            </div>
+        )
+    }
+}
