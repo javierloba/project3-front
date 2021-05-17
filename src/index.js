7683c9ab4169de6ec7389cd848914a33a7d8dd67
Esther Perez Serrajordi <estherperezserrajordi@MacBook-Pro-de-Esther.local>
2021-05-17 16:22:13 +0200

    Added context

--- src/index.js
+++ src/index.js
@@ -4,12 +4,21 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import { AuthProvider } from './context/auth.context';
 import { UserProvider } from './context/user.context';
+import { WorkerProvider } from './context/worker.context';
+import { ServiceProvider } from './context/service.context';
+import { ReserveProvider } from './context/reserve.context';
 
 ReactDOM.render(
   <Router>
   <AuthProvider>
     <UserProvider>
-      <App />
+      <WorkerProvider>
+        <ServiceProvider>
+          <ReserveProvider>
+            <App />
+          </ReserveProvider>
+        </ServiceProvider>
+      </WorkerProvider>
     </UserProvider>
   </AuthProvider>
   </Router>
